ecca7c1a722f99be23f7a87a90783e2fd7278727
byron-clever <673424334@qq.com>
2019-09-25 19:37:37 +0800

    py提交

---
+++ test11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#print "How old are you?",
+age = raw_input("How old are you?")
+#print "How tall are you?",
+height = raw_input("How tall are you?")
+#print "How much do you weight?",
+weight = raw_input("How much do you weight?")
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight )
